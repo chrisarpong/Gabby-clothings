857f384c18acbb883deef2452125077052e8e0e1
Christian Sarpong <christiananietie10@gmail.com>
2026-05-28 18:04:16 +0000

    refactor: remove temporary testing scripts and maintenance utilities

--- test-convex.js
+++
@@ -1,7 +0,0 @@
-import { ConvexHttpClient } from "convex/browser";
-import dotenv from "dotenv";
-dotenv.config({ path: ".env.local" });
-
-const client = new ConvexHttpClient(process.env.VITE_CONVEX_URL);
-
-client.query("appointments:getAvailableSlots", { date: "2026-05-30" }).then(console.log).catch(console.error);
